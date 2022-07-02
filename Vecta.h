cb0311d47a8132da1094c1995b737d4ea5b6dcbf
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-02 13:20:25 +0200

    Vecta (header)

---
+++ Vecta.h
@@ -0,0 +1,61 @@
+// Lic:
+// Vecta.h
+// Vecta (header)
+// version: 22.07.02
+// Copyright (C) 2022 Jeroen P. Broks
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software
+// in a product, an acknowledgment in the product documentation would be
+// appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
+// EndLic
+
+// IMPORTANT NOTE!
+// The routine to make it possible to recalculate the time systems is zlib.
+// The Phantasar Time system itself is NOT! It's proparty of Jeroen P. Broks
+// And its usage in your own work is prohibited, unless you do not plan to 
+// release it at all, or will not gain any profit from it in neither, money, nor
+// goods, nor services. And Jeroen P. Broks MUST always be credited.
+
+#ifndef INCLUDED_PHANTASARVECTA
+#define INCLUDED_PHANTASARVECTA
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const unsigned int Earth_Etmaal = 86400;
+static const unsigned int Phantasar_Etmaal = 20000;
+
+typedef struct TimeEarth {
+	unsigned char
+		Hour,
+		Minute,
+		Second;
+} TimeEarth;
+
+typedef struct TimePhantsar {
+	unsigned int Millo;
+	unsigned char Vecta;
+} TimePhantasar;
+
+TimeEarth PhanToEarth(unsigned char v, unsigned int m);
+TimeEarth PhanToEarth2(TimePhantasar t);
+
+TimePhantasar EarthToPhan(unsigned char h, unsigned char m, unsigned char s);
+TimePhantasar EarthToPhan2(TimeEarth t);
+
+#ifdef __cplusplus
+}
+
+static TimePhantasar EarthToPhan(TimeEarth t) { EarthToPhan2(t); }
+static TimeEarth PhanToEarth(TimePhantasar t) { PhanToEarth(t); }
+#endif
+#endif
